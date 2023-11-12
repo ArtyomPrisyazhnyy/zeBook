efb7dd346fcba806df0f7e3d52cc47b3ddbff150
Artyom Prisyazhnyy <77559081+ArtyomPrisyazhnyy@users.noreply.github.com>
2023-11-12 05:28:07 +0300

    Delete server/node_modules directory

--- server/node_modules/@nest/core/decorators/http/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './request-mapping.decorator';
-export * from './route-params.decorator';
-export * from './http-code.decorator';
-export * from './create-route-param-metadata.decorator';
